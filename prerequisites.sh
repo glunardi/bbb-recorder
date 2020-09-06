6ac9fe7d198c7c6fa702af5c89dce9cdd0f612e6
Guy Lunardi <glunardi@gmail.com>
2020-09-05 23:37:59 -0400

    Removes running systemctl status as it pauses the script

--- prerequisites.sh
+++ prerequisites.sh
@@ -4,7 +4,6 @@ add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(
 apt-get update
 apt-cache policy docker-ce
 apt-get install -y docker-ce
-systemctl status docker
 curl -L https://github.com/docker/compose/releases/download/1.26.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 docker-compose --version
